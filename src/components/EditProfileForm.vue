5458a6460d6f8a7cb9f2839c272c0a910845f728
AnnaAnnaNX <anna.anna.annanx@rambler.ru>
2024-03-02 14:53:15 +0300

    add avatar

--- src/components/EditProfileForm.vue
+++ src/components/EditProfileForm.vue
@@ -10,14 +10,34 @@
       Основная информация
     </p>
     <v-divider></v-divider>
-    <div>
-      <v-img v-if="image" :src="image" />
+    <div style="display: flex; justify-content: center; align-items: center; flex-direction: column;">
+      <div
+        :style="{background: image ? `url(${image})` : '#c4c4c4'}"
+        class="avatar"
+      >
+        <div
+          v-if="!image"
+          style="font-size: 30px;"
+        >АБ</div>
+      </div>
       <p>Перетащите или выберети фото на компьютере</p>
       <v-file-input
-        :label="chosenFile ? '' : 'Выбрать файл'"
+        clearable
         v-model="chosenFile"
         @change="onFileChange"
-      ></v-file-input>
+        base-color="white"
+        bg-color="white"
+        color="white"
+        @click:clear="clear"
+        class="file-input"
+      >
+        <template v-slot:label></template>
+        <template v-slot:selection></template>
+        <template v-slot:prepend-inner>
+          <div v-if="!image" class="title">Выберите файл</div>
+          <div v-if="image" class="title">{{fileName}}</div>
+        </template>
+      </v-file-input>
     </div>
   </div>
 </template>
@@ -27,18 +47,41 @@ import { ref } from 'vue'
 
 const chosenFile = ref(null)
 const image = ref(null)
+const fileName = ref('')
 
 const onFileChange = function(file) {
     const reader = new FileReader()
     reader.readAsDataURL(chosenFile.value[0])
+    fileName.value = chosenFile.value[0].name
     reader.onload = () => {
-        image.value = reader.result
+      image.value = reader.result
     }
+}
+const clear = () => {
+  image.value = null
+  fileName.value = ''
 }
 </script>
 
-<style scoped>
+<style>
 .button-text {
   text-transform: capitalize;
+}
+.file-input {
+  background-color: white!important;
+}
+.title {
+  min-width: 300px;
+  color: orange;
+}
+.avatar {
+  width: 200px;
+  height: 200px;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: grey;
+  font-size: 70px;
 }
 </style>
