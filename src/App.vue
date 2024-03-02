9acab67801d6c6855f54e15ebefd172433626490
AnnaAnnaNX <anna.anna.annanx@rambler.ru>
2024-03-02 16:21:05 +0300

    add data picker

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,18 @@
 <template>
   <v-app>
     <v-main>
-        <v-container fluid class="fill-height" style="padding:0;">
-            <v-row class="fill-height" style="flex-wrap: nowrap;">
-              <v-col cols="3" style="background: #212121; min-width: 200px;">
+        <v-container fluid class="fill-height" style="padding:0; margin: 0;">
+            <v-row class="fill-height" style="flex-wrap: nowrap; margin: 0;">
+              <v-col cols="3" style="background: #212121; min-width: 200px; margin: 0;">
                 <left-menu />
               </v-col>
-              <v-col cols="9" style="min-width: 200px; padding: 0;">
+              <v-col cols="9" style="min-width: 200px; padding: 0; margin: 0;">
                 <top-menu />
-                <v-row style="flex-wrap: nowrap;">
-                  <v-col cols="8">
+                <v-row style="flex-wrap: nowrap; margin: 0;">
+                  <v-col cols="8" style="margin: 0;">
                     <edit-profile-form />
                   </v-col>
-                  <v-col cols="4">
+                  <v-col cols="4" style="margin: 0;">
                     <right-menu />
                   </v-col>
                 </v-row>
--- src/components/EditProfileForm.vue
+++ src/components/EditProfileForm.vue
@@ -19,6 +19,7 @@
       </div>
       <p>Перетащите или выберети фото на компьютере</p>
       <v-file-input
+        accept="image/png, image/jpeg, image/bmp"
         clearable
         v-model="chosenFile"
         @change="onFileChange"
@@ -39,17 +40,107 @@
 
     <div>
       <v-row>
-        <v-col cols="6">
+        <v-col cols="6" class="col">
           <label>Фамилия</label>
-          <v-text-field v-model="form.name"></v-text-field>
+          <v-text-field v-model="form.name" variant="outlined"></v-text-field>
         </v-col>
-        <v-col cols="6">
+        <v-col cols="6" class="col">
           <label>Имя</label>
-          <v-text-field v-model="form.firstName"></v-text-field>
+          <v-text-field v-model="form.firstName" variant="outlined"></v-text-field>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col class="col">
+          <label>Статус</label>
+          <v-select
+              clearable
+              :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']"
+              variant="outlined"
+              v-model="form.status"
+          ></v-select>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col class="col">
+          <label>Гражданство</label>
+          <v-select
+              clearable
+              :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']"
+              variant="outlined"
+              v-model="form.country"
+          ></v-select>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col class="col">
+          <label>Готовность переехать</label>
+          <v-select
+              clearable
+              :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']"
+              variant="outlined"
+              v-model="form.readyRelocate"
+          ></v-select>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col class="col">
+          <label>Дата рождения</label>
+          <v-text-field @click="showCalender = !showCalender" :value="form?.date?.toISOString()?.split('T')?.[0]" variant="outlined"></v-text-field>
+          <v-date-picker v-if="showCalender" v-model="form.date" @update:modelValue="showCalender = false"></v-date-picker>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col class="col">
+          <label>Основная специальность</label>
+          <v-text-field v-model="form.readyRelocate" variant="outlined"></v-text-field>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col class="col">
+          <v-switch label="Стропальщик" v-model="form.slinger" color="orange"></v-switch>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="6" class="col">
+          <label>Желаемая заработная плата</label>
+          <v-text-field v-model="form.salary" variant="outlined"></v-text-field>
+        </v-col>
+        <v-col cols="6" class="col">
+          <label>Желаемая часовая ставка</label>
+          <v-text-field v-model="form.salaryPerHour" variant="outlined"></v-text-field>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="6" class="col">
+          <label>График работы</label>
+          <v-select
+              clearable
+              :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']"
+              variant="outlined"
+              v-model="form.schedule"
+          ></v-select>
+        </v-col>
+        <v-col cols="6" class="col">
+          <label>Желаемая выработка часов</label>
+          <v-text-field v-model="form.hours" variant="outlined"></v-text-field>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col class="col">
+          <label>О себе</label>
+          <v-textarea v-model="form.about" variant="outlined"></v-textarea>
+          <div style="font-size: 10px; text-align: right; margin-top: -10px;">
+            {{form?.about?.length || 0}}/{{500}}
+          </div>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col class="col" style="display: flex; justify-content: flex-end; margin-top: 10px;">
+          <v-btn>Отмена</v-btn>
+          <v-btn @click.prevent="save" color="orange" style="color: white; margin-left: 10px;">Сохранить</v-btn>
         </v-col>
       </v-row>
     </div>
-{{form}}
   </div>
 </template>
 
@@ -63,6 +154,7 @@ const form = ref({})
 const avatarText = computed(() => {
   return `${form.value?.name?.[0] || ''}${form.value?.firstName?.[0] || ''}`
 })
+const showCalender = ref(false)
 
 const onFileChange = function(file) {
     const reader = new FileReader()
@@ -75,6 +167,14 @@ const onFileChange = function(file) {
 const clear = () => {
   image.value = null
   fileName.value = ''
+}
+
+const save = () => {
+  const value = {
+    ...form.value,
+    file: chosenFile.value
+  }
+  console.log(value)
 }
 </script>
 
@@ -99,5 +199,9 @@ const clear = () => {
   color: grey;
   font-size: 70px;
   text-transform: uppercase;
+}
+.col {
+  padding-top: 0;
+  padding-bottom: 0;
 }
 </style>
